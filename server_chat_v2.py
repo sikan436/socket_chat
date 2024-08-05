51c5ade79bd970d7c467a89b237b3083e423eb18
sikan sikan <sikan436@gmail.com>
2024-08-05 14:52:37 +0530

    v2 commit

---
+++ server_chat_v2.py
@@ -0,0 +1,23 @@
+import socket 
+import sys
+
+s=socket.socket()
+print ("socket connected succesfully")
+
+port = 12345  
+s.bind(('',port))
+print (f"socket binded to {port} successfully")
+
+s.listen(5)
+
+print ("socket listening")
+
+while True:
+    c,addr=s.accept()
+    name=input("type your message here")
+    print (f"got connection from {addr}")
+    c.send(f'Thank you for connecting {name}'.encode()) 
+    c.close()
+    break
+
+ 
